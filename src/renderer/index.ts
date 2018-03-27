ee48c3df273a8265e93f5105c888106768422556
Joe Berria <joeberria@gmail.com>
2018-03-26 22:57:13 -0700

    moving vertexattribute data to the renderable class

--- src/renderer/index.ts
+++ src/renderer/index.ts
@@ -1,12 +1,37 @@
-import { ShaderType } from '../shader/shader';
+import { ShaderType, IVertexAttribute } from '../shader';
 import { Scene } from '../scene/scene';
 
 export { WebGLRenderer } from '../renderer/webGLRenderer';
 
 export interface IRenderable {
+	/**
+	 * set the color of the renderable object
+	 * @param {number} r	the red value
+	 * @param {number} g	the green value
+	 * @param {number} b	the blue value
+	 * @param {number} a	the alpha value
+	 */
 	setColor(r: number, g: number, b: number, a: number): void;
+
+	/**
+	 * set the renderable's texture
+	 */
 	setTexture(): void;
-	bindBuffer(gl: WebGLRenderingContext): void;
+
+	/**
+	 * must be called at lease once before rendering the renderable.
+	 * This will bind and describe the buffer
+	 * @param {WebGLRenderingContext}	gl	webGLrendering context
+	 * @param {WebGLProgram}	program	the program id for the shaders
+	 */
+	prepareBuffer(gl: WebGLRenderingContext, program: WebGLProgram): void;
+
+	/**
+	 * describe the attribute data for the renderable.
+	 * @param {string}	attName	the attribute name found in the shader
+	 * @param {IVertexAttribute}	attribute	the attribute object describing the renderable
+	 */
+	setVertexAttributeFor(attName: string, attribute: IVertexAttribute): void;
 }
 
 export interface IRenderer {
