f6256953a0a7ddb4c0824008d26105e1aea71beb
Joe Berria <joeberria@gmail.com>
2018-04-16 16:30:04 -0700

    removing lib files from git.

--- lib/types/agent.d.ts
+++
@@ -1,6 +0,0 @@
-export { Square } from './geometry/square';
-export { Scene } from './scene/scene';
-export { WebGLRenderer } from './renderer';
-export { OrthographicCamera, PerspectiveCamera } from './camera';
-export { default as Shader, ShaderType } from './shader';
-export * from './math';
