f89aea1495030bfc52daf609ebc30db24a02d78b
Joe Berria <joeberria@gmail.com>
2018-09-19 23:08:58 -0700

    initial commit for shader var refactor

---
+++ src/shader/attribute.ts
@@ -0,0 +1,19 @@
+
+export class Attribute {
+
+  constructor() {
+    // TODO
+  }
+
+  public set(type: string, name: string) {
+    // TODO
+  }
+
+  public clear() {
+    // TODO
+  }
+
+  public preRender(gl: WebGLRenderingContext) {
+    // TODO
+  }
+}
---
+++ src/shader/uniform.ts
@@ -0,0 +1,19 @@
+
+export class Uniform {
+
+  constructor() {
+    // TODO
+  }
+
+  public set(type: string, name: string) {
+    // TODO
+  }
+
+  public clear() {
+    // TODO
+  }
+
+  public preRender(gl: WebGLRenderingContext) {
+    // TODO
+  }
+}
