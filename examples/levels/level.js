4b58d2cca94b9a73e1a4e0a09f8d2fb7f48f8034
Joe Berria <joeberria@gmail.com>
2019-01-20 22:52:42 -0800

    removing shader code from example to use the default shader

--- examples/levels/level.js
+++ examples/levels/level.js
@@ -1,33 +1,9 @@
 // @ts-nocheck
-const vshr = `
-attribute vec2 aPos;
-attribute vec2 aText;
-uniform mat4 camera;
-
-varying vec2 vTexture;
-
-void main() {
-  vTexture = aText;
-  gl_Position = camera * vec4(aPos, 0.0, 1.0);
-}
-`;
-
-const fshr = `
-precision mediump float;
-
-uniform sampler2D uSampler;
-varying vec2 vTexture;
-
-void main() {
-  gl_FragColor = texture2D(uSampler, vTexture);
-}
-`;
 
 function runExample() {
   let engine = new Agent.Engine({width: 1200, height: 880});
   let background = new Agent.Mesh(0, 0, {rowLength: 10, columnLength: 10, width: 128, height: 128});
   let camera = new Agent.OrthographicCamera(0, 0, 800, 600);
-  let myShader = new Agent.Shader();
 
   let tile = new Agent.Tile(10, 10, 64, 64);
 
@@ -36,18 +12,12 @@ function runExample() {
   let leveldata = Agent.stringToTextureJSON(JSON.stringify(levelOne));
 
   worldSpriteSheet.loadResourceWithData("../images/groundtilesheet.png", leveldata)
-    .then((success) => {
+    .then(() => {
       background.setSpriteSheet(worldSpriteSheet); // assign our texture to a renderable
     })
-    .catch((success) => {
+    .catch(() => {
       console.log("world spritesheet failed to load");
     });
-
-
-  myShader.setSource(vshr, fshr);
-  myShader.setUniformData("camera", camera.uniform);
-  myShader.setAttributeData("aPos", [ background.vertexAttributes(), tile.vertexAttributes() ]);
-  myShader.setAttributeData("aText", background.textureAttributes());
 
   engine.input.keyDownEvent((e) => {
     if (e.key === "s") {
@@ -61,7 +31,6 @@ function runExample() {
     }
   });
 
-  scene.setShader(myShader);
   scene.addDrawable(background, tile);
   scene.addCamera(camera);
   engine.run();
